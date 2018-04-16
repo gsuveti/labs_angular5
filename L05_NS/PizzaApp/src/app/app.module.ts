64cdb6e0d4d17f68b377560e14774377879bb40c
georgesuveti <George@1r14n>
2018-04-17 01:21:12 +0300

    service init

---
+++ L05_NS/PizzaApp/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppComponent} from './app.component';
+import {TabsComponent} from "./tabs/tabs.component";
+import {FormsModule} from "@angular/forms";
+import {ReviewsComponent} from "./reviews/reviews.component";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TabsComponent,
+    ReviewsComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
