569d4d66e873ca75466d4fdf5fa2f289ea3a3c7d
georgesuveti <George@1r14n>
2018-05-15 11:09:48 +0300

    ng_forms_reactive: change order model

--- PizzaApp/src/app/order/order.model.ts
+++ PizzaApp/src/app/order/order.model.ts
@@ -2,10 +2,10 @@
  * Created by marius on 15/03/2017.
  */
 import {ICustomer} from './customer.model';
-import {IPizza} from './pizza.model';
+import {IPizzaOrder} from './pizza-order.model';
 
 export interface IOrder {
   _id?: string;
   customer: ICustomer;
-  pizzas: IPizza[];
+  pizzaOrders: IPizzaOrder[];
 }
